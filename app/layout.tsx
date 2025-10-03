566cfdee6267dfbdbbcea7ea3941b390dae64e92
lschneiderny <163238998+lschneiderny@users.noreply.github.com>
2025-10-03 00:29:16 -0400

    stripping down

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Providers from "@/components/Providers";
 import { SpeedInsights } from "@vercel/speed-insights/next"
@@ -30,7 +29,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Providers>
-          <Header />
           <main className="min-h-screen">{children}</main>
           <Footer />
         </Providers>
